09328ce580d520ea810da185b0e2b905e5650758
Matheus Cavalcanti <matheus_cla@hotmail.com>
2024-03-18 11:33:21 -0300

    Seven Commit: Inicio da Implementação do código da page de Registro + Validação dos dados informados no campo de input + verificação de usuário já inserido + persistencia no localStorage.

--- js/login.js
+++ js/login.js
@@ -1,39 +1,87 @@
-const btnSalvar = document.querySelector("#btnSalvar");
-let meusRegistrosUsers = [];
+const formUser = document.getElementById("formUser");
+let meusRegistrosUsers = new Array();
 
-btnSalvar.addEventListener("click", function(event){
-  event.preventDefault();
-  console.log("clicado");
-  const frmUser = document.querySelector("#formUser");
-  const fName = frmUser.firstName.value;
-  const lName = frmUser.lastName.value;
-  const bDateUser = frmUser.birthDate.value;
-  const countryUser = frmUser.countryUser.value;
-  const cityUser = frmUser.cityUser.value;
-  const emailUser = frmUser.emailUser.value;
-  const passwordUser = frmUser.passwordUser.value;
-
-  const loginUser = new Login(emailUser, passwordUser);
-  const pessoa = new Pessoa(fName, lName, bDateUser, countryUser, cityUser, loginUser);
-  meusRegistrosUsers.push(pessoa);
-  console.log(meusRegistrosUsers);
-})
-
-/*function buttonAction() {
-  const firstName = document.getElementById("#first-Name".value);
-  const lastName = document.getElementById("#last-Name").value;
-  const birthDate = document.getElementById("#birth-date").value;
-  const country = document.getElementById("#country").value;
-  const city = document.getElementById("#city").value;
-  const email = document.getElementById("#email").value;
-  const password = document.getElementById("#password").value;
-
-  const login = new Login(email, password);
-  const pessoa = new Pessoa(firstName, lastName, birthDate, country, city, login);
-  meusRegistrosUsers.push(pessoa);
+function verificaNomes(a) {
+  const apenasLetras = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s']+$/; //Permite que na String tenha Toda e Qualquer letra, com ou sem Assento.
+  const wordValid = apenasLetras.test(a);
+  return wordValid;
 }
-*/
 
+function verificaSenha(b) {
+  const verificaSenha = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/; //No minimo 08 caracteres, pelos menos 1 letra, um numero e não permite caracter.
+  const passwordValid = verificaSenha.test(b);
+  return passwordValid;
+}
+
+function verificaData(c) {
+  const validaDataNasc =
+    /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  const dateValid = validaDataNasc.test(c);
+  return dateValid;
+}
+
+function verificaEmail(d){
+  const validaEmail = /\S+@\S+\.\S+/;
+  const emailValid =  validaEmail.test(d);
+  return emailValid;
+}
+
+formUser.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const fName = document.getElementById("first-name").value;
+  const lName = document.getElementById("last-name").value;
+  const bDateUser = document.getElementById("birth-date").value;
+  const countryUser = document.getElementById("country").value;
+  const cityUser = document.getElementById("city").value;
+  const emailUser = document.getElementById("email").value;
+  const passwordUser = document.getElementById("password").value;
+
+  document.getElementById("first-name").value = "";
+  document.getElementById("last-name").value = "";
+  document.getElementById("birth-date").value = "";
+  document.getElementById("country").value = "";
+  document.getElementById("city").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+  document.getElementById("password-confirmation").value = "";
+
+  const login = new Login(emailUser, passwordUser);
+  const pessoa = new Pessoa(fName, lName, bDateUser, countryUser, cityUser, login);
+
+  if(verificaNomes(fName) == true){
+    if(verificaNomes(lName) == true){
+      if(verificaData(bDateUser) == true){
+        if(verificaNomes(cityUser)){
+          if(verificaEmail(emailUser)){
+            if(verificaSenha(passwordUser)){
+              if(localStorage.hasOwnProperty("meusRegistrosUsers")){
+                console.log("NÃO FOI CRIADO!")
+                meusRegistrosUsers = JSON.parse(localStorage.getItem("meusRegistrosUsers"));
+              } else{
+                console.log("JÁ CRIADO! SÓ ADD!")
+                meusRegistrosUsers.push(pessoa);
+                localStorage.setItem("meusRegistrosUsers", JSON.stringify(meusRegistrosUsers));
+              }
+            } else{
+              console.log("senha deu errado!");
+            }
+          } else{
+            console.log("deu errado 02");
+          }
+        } else{
+          console.log("deu errado 03");
+        }
+      } else{
+        console.log("deu errado 04");
+      }
+    } else{
+      console.log("deu errado 05");
+    }
+  } else{
+    console.log("deu errado 06");
+  }
+});
 
 class Login {
   constructor(email, password) {
